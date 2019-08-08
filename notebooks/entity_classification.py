326b3ccc6f54be2d89f3ceee3bad120022acfae3
Allan Wright <allan.s.wright@gmail.com>
2019-08-08 09:15:57 +0000

    Explore format of data for named entity recognition

--- notebooks/entity_classification.py
+++ notebooks/entity_classification.py
@@ -3,31 +3,16 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv('data/interim/combined.csv')
+
+#%%
+df = pd.DataFrame(df['name'].str.split().tolist(), index=df.index).stack()
+df = df.reset_index([0, 1])
+df.columns = ['index', 'pos', 'word']
+df['entity'] = ''
 print(df.head())
 
 #%%
-new_df = pd.DataFrame(df['name'].str.split().tolist(), index=df.index).stack()
-print(new_df.shape)
-print(new_df.head())
+df.loc[df.word.str.contains('^s\d+e\d+$'), 'entity'] = 'season_episode'
 
 #%%
-new_df.to_csv('data/interim/entity_classification.csv', index=True)
-
-# Define entity classes by type
-# SHARED
-# na, file extension
-
-# APP
-# name, version
-
-# MOVIE
-# name, resolution, encoding, uploader
-
-# MUSIC
-# artist name, album name, track name, track number, bitrate
-
-# TV
-# show name, episode name, season number, episode number, resolution
-# encoding, uploader
-
-#%%
+df.to_csv('data/interim/entity_classification.csv', index=False)
