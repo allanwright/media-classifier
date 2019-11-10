2dd3d354c15e1f6f03d31623dd29beb70df535ab
Allan Wright <allan.s.wright@gmail.com>
2019-11-10 09:37:00 +0000

    Refactor ner training code

--- src/models/ner.py
+++ src/models/ner.py
@@ -11,81 +11,35 @@ def train():
     '''Trains the named entity recognition model.
 
     '''
-    model = None
-    new_model_name='new_model'
-    output_dir='models/'
-    n_iter=10
+    iterations=10
 
-    LABEL = ['title', 'source', 'season_id', 'episode_id',
-            'resolution', 'episode_name', 'extension',
-            'encoding', 'year']
-    
-    TRAIN_DATA = persistence.bin_to_obj('data/processed/ner_labelled.pickle')
-
-    """ with open ('data/processes/spacy.pickle', 'rb') as fp:
-        TRAIN_DATA = pickle.load(fp) """
+    # Load training data
+    train_data = persistence.bin_to_obj('data/processed/ner_labelled.pickle')
 
     nlp = spacy.blank('en')
     ner = nlp.create_pipe('ner')
     nlp.add_pipe(ner, last=True)
 
-    """ for i in LABEL:
-        ner.add_label(i) """
-
-    for _, annotations in TRAIN_DATA:
+    # Add labels
+    for _, annotations in train_data:
         for ent in annotations.get("entities"):
             ner.add_label(ent[2])
-
-    optimizer = nlp.begin_training()
 
     # get names of other pipes to disable them during training
     other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
     with nlp.disable_pipes(*other_pipes):  # only train NER
-        # reset and initialize the weights randomly – but only if we're
-        # training a new model
-        if model is None:
-            nlp.begin_training()
-        for itn in range(n_iter):
-            random.shuffle(TRAIN_DATA)
+        nlp.begin_training()
+        for itn in range(iterations):
+            random.shuffle(train_data)
             losses = {}
             # batch up the examples using spaCy's minibatch
-            batches = minibatch(TRAIN_DATA, size=compounding(4.0, 32.0, 1.001))
+            batches = minibatch(train_data, size=compounding(4.0, 32.0, 1.001))
             for batch in batches:
                 texts, annotations = zip(*batch)
-                nlp.update(
-                    texts,  # batch of texts
-                    annotations,  # batch of annotations
-                    drop=0.5,  # dropout - make it harder to memorise data
-                    losses=losses,
-                )
+                nlp.update(texts, annotations, drop=0.5, losses=losses)
             print("Losses", losses)
-
-    # test the trained model
-    """ for text, _ in TRAIN_DATA:
-        doc = nlp(text)
-        print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-        print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc]) """
-
-    for s in ['dark s01 e01 mp4', 'back to the future i mkv']:
-        doc = nlp(s)
-        print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-        print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
-
-    # save model to output directory
-    if output_dir is not None:
-        output_dir = Path(output_dir)
-        if not output_dir.exists():
-            output_dir.mkdir()
-        nlp.to_disk(output_dir)
-        print("Saved model to", output_dir)
-
-        # test the saved model
-        print("Loading from", output_dir)
-        nlp2 = spacy.load(output_dir)
-        """ for text, _ in TRAIN_DATA:
-            doc = nlp2(text)
-            print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-            print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc]) """
+    
+    nlp.to_disk('models/ner')
 
 def predict(filename):
     ''' Makes a prediction using the named entity recognition model.
@@ -98,7 +52,7 @@ def predict(filename):
     #df = pd.DataFrame()
     #df['name'] = x_train
     #df['entity'] = ''
-    nlp = spacy.load(Path('models/'))
+    nlp = spacy.load(Path('models/ner'))
     doc = nlp(filename)
     print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
     print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
