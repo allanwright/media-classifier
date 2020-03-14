e3b8cbc7220c0cf0963e8a4331b6d39fbe011e03
Allan Wright <allan.s.wright@gmail.com>
2020-03-14 00:58:30 +0000

    Update docstring

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+'''
+Machine learning project for filename based media classification and named
+entity recognition.
+'''
+
 from setuptools import setup
 
 setup(name='src',
-    packages=['src'],
-    version='0.2.1',
-    entry_points={
-        'console_scripts': ['mc=src.cli:main']
-    }
-)
+      packages=['src'],
+      version='0.2.1',
+      entry_points={'console_scripts': ['mc=src.cli:main']})
