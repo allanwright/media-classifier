6230eb0c3ef041304ed4d8cf06a8ac1a4659b2c3
Allan Wright <allan.s.wright@gmail.com>
2020-03-19 09:47:32 +0000

    Fix linting issues

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,26 +1,26 @@
-''' Defines a data processing pipeline.
+'''Defines a data processing pipeline.
 
 '''
 
 class Pipeline():
-    ''' Defines a data processing pipeline.
+    '''Defines a data processing pipeline.
 
     '''
 
     def __init__(self):
-        ''' Initializes a new instance of the Pipeline object.
+        '''Initializes a new instance of the Pipeline object.
 
         '''
         self.steps = []
 
     def add_step(self, step):
-        ''' Adds a step to the pipeline.
+        '''Adds a step to the pipeline.
 
         '''
         self.steps.append(step)
 
     def run(self):
-        ''' Runs the pipeline.
+        '''Runs the pipeline.
 
         '''
         for step in self.steps:
