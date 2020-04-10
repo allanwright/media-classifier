fb1bab0651623260147587d6bd7786e92dee21b0
Allan Wright <allan.s.wright@gmail.com>
2020-04-10 09:46:30 +0000

    Add timer decorater that can execute function multiple times

--- src/diagnostics.py
+++ src/diagnostics.py
@@ -5,19 +5,53 @@
 import time
 from functools import wraps
 
-def stopwatch(func):
-    '''Times the execution of the specified function.
+def timer(func):
+    '''Times the execution of a function.
 
     Args:
-        func (function): The function to time.
+        func: (callable): The function to execute.
     '''
     @wraps(func)
     def wrapper(*arg, **kw):
-        start = time.time()
-        result = func(*arg, **kw)
-        stop = time.time()
-        print('{func} executed in {elapsed:.4f}ms.'.format(
+        result, exec_time = _time_func(func, *arg, **kw)
+        print('{module}.{func}() executed in {time:,.2f}s.'.format(
+            module=func.__module__,
             func=func.__name__,
-            elapsed=(stop - start) * 1000))
+            time=exec_time))
         return result
     return wrapper
+
+def multi_timer(count: int = 1):
+    '''Times the execution of a function.
+
+    Args:
+        func: (callable): The function to execute.
+        count (int): The number of times to execute the function.
+    '''
+    if count == 0:
+        raise ValueError(count)
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*arg, **kw):
+            times = []
+            for _ in range(count):
+                result, exec_time = _time_func(func, *arg, **kw)
+                times.append(exec_time)
+            print('{module}.{func}() executed {count} times ' \
+                  '[min={min:,.2f}s, max={max:,.2f}s, avg={avg:,.2f}s].'.format(
+                      module=func.__module__,
+                      func=func.__name__,
+                      count=count,
+                      min=min(times),
+                      max=max(times),
+                      avg=sum(times) / len(times)))
+            return result
+        return wrapper
+    return decorator
+
+def _time_func(func, *arg, **kw):
+    start = time.time()
+    result = func(*arg, **kw)
+    stop = time.time()
+    return (result, stop - start)
