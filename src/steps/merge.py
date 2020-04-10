7d453b035903d45c05fd35ee677169112cb94e99
Allan Wright <allan.s.wright@gmail.com>
2020-04-10 09:45:21 +0000

    Remove timer from merge step

--- src/steps/merge.py
+++ src/steps/merge.py
@@ -7,7 +7,6 @@ import os
 import pandas as pd
 import progressbar as pb
 
-from src.diagnostics import stopwatch
 from src.step import Step
 
 class Merge(Step):
@@ -27,7 +26,6 @@ class Merge(Step):
             'path': 'data/interim/combined.csv',
         }
 
-    @stopwatch
     def run(self):
         '''Runs the pipeline step.
 
