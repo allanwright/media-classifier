0ef3155b601e59f7b296edef453461d20382cb81
Allan Wright <allan.s.wright@gmail.com>
2020-05-12 06:06:59 +0000

    Fix linting issues

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,24 +6,22 @@ from src.pipelines.process import Process
 from src.steps.merge import Merge
 from src.steps.prepare_classification_data import PrepareClassificationData
 
-# TODO: Fix docstring spacing
-
 def process_all():
-    ''' Performs all data processing steps.
+    '''Performs all data processing steps.
 
     '''
     pipeline = Process()
     pipeline.run()
 
 def process_merge():
-    ''' Performs merging of data.
+    '''Performs merging of data.
 
     '''
     merge = Merge()
     merge.run()
 
 def process_feature():
-    ''' Performs feature generation.
+    '''Performs feature generation.
 
     '''
     prepare = PrepareClassificationData()
