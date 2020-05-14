33bfd97147972fbcb5fdb03f5ef7950f3045cb6e
Allan Wright <allan.s.wright@gmail.com>
2020-05-14 05:25:55 +0000

    Rename pipeline

---
+++ src/pipelines/aquire_data.py
@@ -0,0 +1,18 @@
+'''Defines a processing pipeline that aquires prediction data.
+
+'''
+
+from src.pipeline import Pipeline
+from src.steps.get_prediction_data import GetPredictionData
+
+class AquireData(Pipeline):
+    '''Defines a processing pipeline that aquires training data.
+
+    '''
+
+    def __init__(self):
+        '''Initializes a new instance of the AquireData object.
+
+        '''
+        super(AquireData, self).__init__()
+        self.add_step(GetPredictionData())
