050b960833729804339e2079114e5c42f164d654
Allan Wright <allan.s.wright@gmail.com>
2020-07-16 10:48:30 +0000

    Add ner validation pipeline step

--- src/pipelines/train_ner.py
+++ src/pipelines/train_ner.py
@@ -4,6 +4,7 @@
 
 from src.pipeline import Pipeline
 from src.steps.train_ner_model import TrainNerModel
+from src.steps.validate_ner import ValidateNer
 
 class TrainNer(Pipeline):
     '''Defines a processing pipeline that trains the ner model.
@@ -13,3 +14,4 @@ class TrainNer(Pipeline):
     def __init__(self):
         super(TrainNer, self).__init__()
         self.add_step(TrainNerModel())
+        self.add_step(ValidateNer())
---
+++ src/steps/validate_ner.py
@@ -0,0 +1,64 @@
+'''Defines a pipeline step which validates the ner model.
+
+'''
+
+import ast
+
+import pandas as pd
+
+from mccore import EntityRecognizer
+from mccore import ner
+from mccore import persistence
+
+from src.step import Step
+
+class ValidateNer(Step):
+    '''Defines a pipeline step which validates the ner model.
+
+    '''
+
+    def __init__(self):
+        super(ValidateNer, self).__init__()
+        self.input = {
+            'predictions': 'data/test/ner1.csv',
+            'model': 'models/ner_mdl.pickle',
+        }
+        self.output = {
+        }
+
+    def run(self):
+        '''Runs the pipeline step.
+
+        '''
+        nlp, _ = ner.get_model()
+        nlp.from_bytes(persistence.bin_to_obj(self.input['model']))
+        recognizer = EntityRecognizer(nlp)
+
+        def predict(row):
+            return recognizer.predict(row['name'])
+
+        df = pd.read_csv(self.input['predictions'])
+        df['actual'] = df.apply(predict, axis=1)
+
+        def print_incorrect(row):
+            actual_list = list(row['actual'])
+            expected_list = list(ast.literal_eval(row['expected']))
+
+            if len(actual_list) != len(expected_list):
+                self.print(
+                    '\'{name}\' [ expected: {expected}, actual: {actual} ]',
+                    name=row['name'],
+                    expected=row['expected'],
+                    actual=row['actual'])
+            else:
+                for i in range(len(actual_list)): # pylint: disable=consider-using-enumerate
+                    x = actual_list[i]
+                    y = expected_list[i]
+                    if x[0] != y[0] or x[1] != y[1]:
+                        self.print(
+                            '\'{name}\' [ expected: {expected}, actual: {actual} ]',
+                            name=row['name'],
+                            expected=y,
+                            actual=x)
+
+        df.apply(print_incorrect, axis=1)
