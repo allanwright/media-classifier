5c92710878ca7fdd21d3aad321e377f60b465fe6
Allan Wright <allan.s.wright@gmail.com>
2020-11-27 08:07:04 +0000

    Add classifier prediction pipeline step

--- src/steps/predict_classifier.py
+++ src/steps/predict_classifier.py
@@ -2,9 +2,7 @@
 
 '''
 
-from mccore import persistence
-from mccore.classifier import Classifier
-
+from src import classifier
 from src.step import Step
 
 class PredictClassifier(Step):
@@ -15,9 +13,6 @@ class PredictClassifier(Step):
     def __init__(self):
         super(PredictClassifier, self).__init__()
         self.input = {
-            'vectorizer': 'models/classifier_vec.pickle',
-            'model': 'models/classifier_mdl.pickle',
-            'label_dict': 'data/processed/label_dictionary.json'
         }
         self.output = {
         }
@@ -26,12 +21,4 @@ class PredictClassifier(Step):
         '''Runs the pipeline step.
 
         '''
-        classifier = Classifier(
-            persistence.bin_to_obj(self.input['vectorizer']),
-            persistence.bin_to_obj(self.input['model']),
-            persistence.json_to_obj(self.input['label_dict'])
-        )
-        label, confidence = classifier.predict(self.input['--filename'])
-
-        print('Predicted class \'{label}\' with {confidence:.2f}% confidence.'
-              .format(label=label, confidence=confidence*100))
+        return classifier.predict(self.input['--filename'])
